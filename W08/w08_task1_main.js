679081884938f07a8eae04b13e866c3b250a2fa0
KentaroUchida <219x003x@stu.kobe-u.ac.jp>
2021-05-14 16:58:35 +0900

    Initial commit

--- W08/w08_task1_main.js
+++ W08/w08_task1_main.js
@@ -1,29 +1,29 @@
-d3.csv("https://KentaroUchida.github.io/InfoVis2021/W08/data.csv")
+d3.csv("https://KentaroUchida.github.io/InfoVis2021/W08/data_task1.csv")
     .then( data => {
-        data.forEach( d => { d.x = +d.x; d.y = +d.y; });
+        data.forEach( d => { d.value = +d.value; });
 
         var config = {
             parent: '#drawing_region',
-            width: 256,
-            height: 256,
-            margin: {top:10, right:10, bottom:20, left:25}
+            width: 512,
+            height: 2046,
+            margin: {top:50, right:10, bottom:50, left:100}
         };
 
-        const scatter_plot = new ScatterPlot( config, data );
-        scatter_plot.update();
+        const bar_chart = new BarChart( config, data );
+        bar_chart.update();
     })
     .catch( error => {
         console.log( error );
     });
 
-class ScatterPlot {
+class BarChart {
 
     constructor( config, data ) {
         this.config = {
             parent: config.parent,
-            width: config.width || 256,
-            height: config.height || 256,
-            margin: config.margin || {top:10, right:10, bottom:10, left:10}
+            width: config.width || 512,
+            height: config.height || 2046,
+            margin: config.margin || {top:50, right:10, bottom:50, left:100}
         }
         this.data = data;
         this.init();
@@ -42,35 +42,52 @@ class ScatterPlot {
         self.inner_width = self.config.width - self.config.margin.left - self.config.margin.right;
         self.inner_height = self.config.height - self.config.margin.top - self.config.margin.bottom;
 
+        // Initialize axis scales
         self.xscale = d3.scaleLinear()
-            .range( [0, self.inner_width] );
+            .range([0, self.inner_width]);
 
-        self.yscale = d3.scaleLinear()
-            .range( [self.inner_height, 0] );
+        self.yscale = d3.scaleBand()
+            .range([0, self.inner_height])
+            .paddingInner(0.1);
 
+        // Initialize axes
         self.xaxis = d3.axisBottom( self.xscale )
-            .ticks(6);
+            .ticks(5)
+            .tickSizeOuter(0);
 
-        self.xaxis_group = self.chart.append('g')
-            .attr('transform', `translate(0, ${self.inner_height})`);
-        
         self.yaxis = d3.axisLeft( self.yscale )
-            .ticks(6);
+            .tickSizeOuter(0);
+
+        // Draw the axis
+        self.xaxis_group = self.chart.append('g')
+            .attr('transform', `translate(0, ${self.inner_height})`)
 
         self.yaxis_group = self.chart.append('g')
-            .attr('transform', `translate(0, 0)`);
+
+        self.chart.append("text")
+            .attr("x", self.inner_width / 2)
+            .attr("y", self.inner_height + self.config.margin.bottom)
+            .text("minimum wages");
+
+        self.chart.append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 0 - self.config.margin.left / 1.5)
+            .attr("x", - self.inner_height / 2)
+            .text("country");
+
+        self.chart.append("text")
+            .attr("x", self.inner_width / 5)
+            .attr("y", 0 - self.config.margin.top / 3)
+            .style("font-size", '24px')
+            .style("font-weight", 'bold')
+            .text("Minimum wages of countries in the world (dollar)");
     }
 
     update() {
         let self = this;
 
-        const xmin = d3.min( self.data, d => d.x );
-        const xmax = d3.max( self.data, d => d.x );
-        const ymin = d3.min( self.data, d => d.y );
-        const ymax = d3.max( self.data, d => d.y );
-
-        self.xscale.domain( [0, Math.max(xmax,ymax)+10] );
-        self.yscale.domain( [0, Math.max(xmax,ymax)+10] );
+        self.xscale.domain( [0, d3.max(self.data, d=> d.value)] );
+        self.yscale.domain(self.data.map(d => d.country));
 
         self.render();
     }
@@ -78,14 +95,15 @@ class ScatterPlot {
     render() {
         let self = this;
 
-        self.chart.selectAll("circle")
-            .data(self.data)
-            .enter()
-            .append("circle")
-            .attr("cx", d => self.xscale( d.x ) )
-            .attr("cy", d => self.yscale( d.y ) )
-            .attr("r", d => d.r );
-
+        // Draw bars
+        self.chart.selectAll("rect").data(self.data).enter()
+            .append("rect")
+            .attr("x", 0)
+            .attr("y", d => self.yscale(d.label))
+            .attr("width", d => self.xscale(d.value))
+            .attr("height", self.yscale.bandwidth())
+            .style("fill", d => d.color);
+        
         self.xaxis_group
             .call( self.xaxis );
         
