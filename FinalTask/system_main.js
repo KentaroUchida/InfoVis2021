9284b5244ae9f4aad92cdaf19aea3cd53144af0e
KentaroUchida <219x003x@stu.kobe-u.ac.jp>
2021-06-13 19:53:55 +0900

    Initial Commit

--- FinalTask/system_main.js
+++ FinalTask/system_main.js
@@ -16,7 +16,7 @@ d3.csv("https://kentarouchida.github.io/InfoVis2021/FinalTask/final_task_data.cs
         bar_chart_cedar = new BarChartCedar( {
             parent: '#drawing_region_barchart_cedar',
             width: 650,
-            height: 310,
+            height: 280,
             margin: {top:10, right:10, bottom:50, left:50},
             xlabel: 'date',
             ylabel: 'Amount of cedar pollen dispersed [/cm^2]',
@@ -25,8 +25,8 @@ d3.csv("https://kentarouchida.github.io/InfoVis2021/FinalTask/final_task_data.cs
 
         scatter_plot_cedar_rain = new ScatterPlotCedarRain( {
             parent: '#drawing_region_scatterplot_cedar_rain',
-            width: 310,
-            height: 310,
+            width: 280,
+            height: 280,
             margin: {top:10, right:10, bottom:50, left:50},
             xlabel: 'Precipitation [mm]',
         }, input_data );
@@ -34,8 +34,8 @@ d3.csv("https://kentarouchida.github.io/InfoVis2021/FinalTask/final_task_data.cs
 
         scatter_plot_cedar_sun = new ScatterPlotCedarSun( {
             parent: '#drawing_region_scatterplot_cedar_sun',
-            width: 310,
-            height: 310,
+            width: 280,
+            height: 280,
             margin: {top:10, right:10, bottom:50, left:50},
             xlabel: 'Daylight hours',
         }, input_data );
@@ -44,7 +44,7 @@ d3.csv("https://kentarouchida.github.io/InfoVis2021/FinalTask/final_task_data.cs
         bar_chart_cypress = new BarChartCypress( {
             parent: '#drawing_region_barchart_cypress',
             width: 650,
-            height: 310,
+            height: 280,
             margin: {top:10, right:10, bottom:50, left:50},
             xlabel: 'date',
             ylabel: 'Amount of cypress pollen dispersed [/cm^2]',
@@ -53,8 +53,8 @@ d3.csv("https://kentarouchida.github.io/InfoVis2021/FinalTask/final_task_data.cs
 
         scatter_plot_cypress_rain = new ScatterPlotCypressRain( {
             parent: '#drawing_region_scatterplot_cypress_rain',
-            width: 310,
-            height: 310,
+            width: 280,
+            height: 280,
             margin: {top:10, right:10, bottom:50, left:50},
             xlabel: 'Precipitation [mm]',
         }, input_data );
@@ -62,8 +62,8 @@ d3.csv("https://kentarouchida.github.io/InfoVis2021/FinalTask/final_task_data.cs
 
         scatter_plot_cypress_sun = new ScatterPlotCypressSun( {
             parent: '#drawing_region_scatterplot_cypress_sun',
-            width: 310,
-            height: 310,
+            width: 280,
+            height: 280,
             margin: {top:10, right:10, bottom:50, left:50},
             xlabel: 'Daylight hours',
         }, input_data );
